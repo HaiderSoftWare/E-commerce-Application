a7b6d7b2596b7f1fb68e3b7fe0685e412c6e4425
Haider <dev.haider.majid@gmail.com>
2022-07-20 02:51:56 +0300

    first commit

---
+++ ecommerce/lib/routes.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce/screens/forgot_password/forgot_password_screen.dart';
+import 'package:ecommerce/screens/login_success/login_success_screen.dart';
+import 'package:ecommerce/screens/otp/otp_screen.dart';
+import 'package:ecommerce/screens/sign_in/sign_in_screen.dart';
+import 'package:ecommerce/screens/splash/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'screens/cart/cart_screen.dart';
+import 'screens/complete_profile/complete_profile_screen.dart';
+import 'screens/details/details_screen.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/profile/profile_screen.dart';
+import 'screens/sign_up/sign_up_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  SplashScreen.routeName: (context) => SplashScreen(),
+  SignInScreen.routeName: (context) => SignInScreen(),
+  ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
+  LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
+  OtpScreen.routeName: (context) => OtpScreen(),
+  HomeScreen.routeName: (context) => HomeScreen(),
+  DetailsScreen.routeName: (context) => DetailsScreen(),
+  CartScreen.routeName: (context) => CartScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
+};
